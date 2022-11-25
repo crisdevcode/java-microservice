7cd9f916783ad1d94551df9351dae33c94e30d5c
Cristian Unigarro <ungcrisfit@gmail.com>
2022-11-25 00:01:47 -0500

    refactor: controller and service

--- src/main/java/com/client/microservice/controller/ClientController.java
+++ src/main/java/com/client/microservice/controller/ClientController.java
@@ -1,8 +1,9 @@
 package com.client.microservice.controller;
 
-import java.util.List; 
+import java.util.List;  
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.client.microservice.config.exception.NotFoundException;
 import com.client.microservice.model.Client;
 import com.client.microservice.service.ClientService;
 
@@ -30,37 +30,24 @@ public class ClientController {
 	@ApiOperation(value = "Retorna todos los clientes existentes", response = List.class)
 	@GetMapping
 	public ResponseEntity<List<Client>> getClients() {
-		List<Client> clients = clientService.getAll();
-		if(clients.isEmpty()) {
-			return ResponseEntity.noContent().build();
-		}
-		return ResponseEntity.ok(clients);		
+		return ResponseEntity.status(HttpStatus.OK).body(clientService.getAll());
 	}
 
 	@ApiOperation(value = "Retorna un cliente por Id", response = Client.class)
 	@GetMapping("/{id}")
 	public ResponseEntity<Client> getClient(@ApiParam(value = "Id del cliente a consultar", required = true) @PathVariable("id") Long id){
-		Client client = clientService.getClientById(id);
-		if(client == null) {
-			throw new NotFoundException("Client with Id " + id + " does not exist");
-		}
-		return ResponseEntity.ok(client);
+		return ResponseEntity.status(HttpStatus.OK).body(clientService.getClientById(id));
 	}
 	
 	@ApiOperation(value = "Retorna un cliente basado en el tipo y número de documento")
 	@GetMapping("/document/{type}/{number}")
 	public ResponseEntity<List<Client>> getClientInfo(@ApiParam(value = "Tipo de documento (C o P)", required = true) @PathVariable String type, @ApiParam(value = "Número de documento", required = true) @PathVariable String number){
-		 List<Client> client = clientService.getClientByDocument(type, number);
-		 if(client == null) {
-			 return ResponseEntity.noContent().build();
-		 }
-		return ResponseEntity.ok(client);
+		return ResponseEntity.status(HttpStatus.OK).body(clientService.getClientByDocument(type, number));	
 	}
 
 	@ApiOperation(value = "Crea un nuevo cliente")
 	@PostMapping
 	public ResponseEntity<Client> saveClient(@RequestBody Client client) {
-		Client newClient = clientService.save(client);
-		return ResponseEntity.ok(newClient);
+		return ResponseEntity.status(HttpStatus.CREATED).body(clientService.save(client));
 	}
 }
--- src/main/java/com/client/microservice/service/ClientService.java
+++ src/main/java/com/client/microservice/service/ClientService.java
@@ -7,6 +7,8 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.client.microservice.config.exception.BadRequestException;
+import com.client.microservice.config.exception.NotFoundException;
 import com.client.microservice.model.Client;
 import com.client.microservice.repository.ClientRepository;
 
@@ -36,16 +38,23 @@ public class ClientService {
 	}
 	
 	public Client getClientById(Long id) {
+		if(!clientRepository.findById(id).isPresent()) {
+			throw new NotFoundException("Client with Id " + id + " does not exist");
+		}
 		return clientRepository.findById(id).orElse(null);
 	}
 	
 	public List<Client> getClientByDocument(String type, String number) {
-		
+		if(clientRepository.getClientByDocument(type, number).isEmpty()) {
+			throw new NotFoundException("Client with the type: \"" + type + "\" and " + " number: \"" + number + "\" of document does not exist.");
+		}
 		return clientRepository.getClientByDocument(type, number);
 	}
 	
 	public Client save(Client client) {
-		Client newClient = clientRepository.save(client);
-		return newClient;
+		if(client.getDocumentNumber() == null || client.getDocumentNumber().isEmpty()) {
+			throw new BadRequestException("Invalid document number");
+		}
+		return clientRepository.save(client);
 	}
 }
