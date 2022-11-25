010a5c3fc28a2e1157a73854c1f438f1e68649b6
Cristian Unigarro <ungcrisfit@gmail.com>
2022-11-24 20:22:15 -0500

    add: swagger documentation

--- src/main/java/com/client/microservice/MicroserviceV1Application.java
+++ src/main/java/com/client/microservice/MicroserviceV1Application.java
@@ -2,7 +2,9 @@ package com.client.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = {"com.client.microservice"})
 @SpringBootApplication
 public class MicroserviceV1Application {
 	
--- src/main/java/com/client/microservice/controller/ClientController.java
+++ src/main/java/com/client/microservice/controller/ClientController.java
@@ -1,9 +1,8 @@
 package com.client.microservice.controller;
 
-import java.util.List;
+import java.util.List; 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,11 +11,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.client.microservice.config.exception.BadRequestException;
 import com.client.microservice.config.exception.NotFoundException;
 import com.client.microservice.model.Client;
 import com.client.microservice.service.ClientService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+@Api(value = "Microservicio de Clientes")
 @RestController
 @RequestMapping("/clients")
 public class ClientController {
@@ -24,6 +27,7 @@ public class ClientController {
 	@Autowired
 	private ClientService clientService;
 	
+	@ApiOperation(value = "Retorna todos los clientes existentes", response = List.class)
 	@GetMapping
 	public ResponseEntity<List<Client>> getClients() {
 		List<Client> clients = clientService.getAll();
@@ -33,8 +37,9 @@ public class ClientController {
 		return ResponseEntity.ok(clients);		
 	}
 
+	@ApiOperation(value = "Retorna un cliente por Id", response = Client.class)
 	@GetMapping("/{id}")
-	public ResponseEntity<Client> getClient(@PathVariable("id") Long id){
+	public ResponseEntity<Client> getClient(@ApiParam(value = "Id del cliente a consultar", required = true) @PathVariable("id") Long id){
 		Client client = clientService.getClientById(id);
 		if(client == null) {
 			throw new NotFoundException("Client with Id " + id + " does not exist");
@@ -42,19 +47,20 @@ public class ClientController {
 		return ResponseEntity.ok(client);
 	}
 	
+	@ApiOperation(value = "Retorna un cliente basado en el tipo y número de documento")
 	@GetMapping("/document/{type}/{number}")
-	public ResponseEntity<List<Client>> getClientInfo(@PathVariable String type, @PathVariable String number){
+	public ResponseEntity<List<Client>> getClientInfo(@ApiParam(value = "Tipo y número del cliente a consultar", required = true) @PathVariable String type, @PathVariable String number){
 		 List<Client> client = clientService.getClientByDocument(type, number);
 		 if(client == null) {
 			 return ResponseEntity.noContent().build();
 		 }
 		return ResponseEntity.ok(client);
 	}
-	
+
+	@ApiOperation(value = "Crea un nuevo cliente recibido en la petición")
 	@PostMapping
 	public ResponseEntity<Client> saveClient(@RequestBody Client client) {
 		Client newClient = clientService.save(client);
 		return ResponseEntity.ok(newClient);
 	}
-	
 }
